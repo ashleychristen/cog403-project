16445693d5647b775675704065e641517319495a
Emily <emily.jennett@mail.utoronto.ca>
2026-03-16 10:08:21 -0400

    fix cleaning again

--- data_cleaning/reorganize_data_standardized.py
+++ data_cleaning/reorganize_data_standardized.py
@@ -5,19 +5,21 @@ import numpy as np
 with open('organized_info.json', 'r') as f:
     data = json.load(f)
 
-features_to_remove = ["10B", "5A", "18A"]
+features_to_remove = ["10B", "5A", "18A", "25A"]
 
 for language in data:
     removal = []
 
     for item in data[language]:
-        
         if not item[0].isnumeric():
             continue
+ 
         value = data[language][item]['value']
 
         if item in features_to_remove:
+            removal.append(item)
             continue
+        
         #4A (Voicing in Plosives and Fricatives)
         if item == "4A":
 
@@ -94,11 +96,11 @@ for language in data:
                 value = 2
 
         #10A (Vowel Nasalization)
-            elif item == "10A":
-                if value == 1:
-                    value = 0
-                elif value == 2:
-                    value = 1
+        elif item == "10A":
+            if value == 1:
+                value = 0
+            elif value == 2:
+                value = 1
             
         #11A (Front Rounded Vowels)
         elif item == "11A":
@@ -168,7 +170,7 @@ for language in data:
 
         #17A (Rhythm Types)
         elif item =="17A":
-            if value == 1 or 2: 
+            if value == 1 or value == 2: 
                 value = 1
 
             elif value == 3:
@@ -249,7 +251,7 @@ for language in data:
                 value = 2
 
             elif value == 5:
-                value = 0
+                removal.append(item)
 
 
         # 24A(Locus of Marking in Possessive Noun Phrases)
@@ -265,7 +267,7 @@ for language in data:
                 value = 2
 
             elif value == 5:
-                value = 0
+                removal.append(item)
 
 
         #25B Zero Marking of A and P Arguments
@@ -341,15 +343,12 @@ for language in data:
             elif value == 3:
                 value = 2
 
-        data[language][item]['value'] = value
-
-for language in data:
-    for feat in features_to_remove:
-        data[language].pop(feat, None)
+    for feat in removal:
+        data[language].pop(feat)
 
 #normalizing
 
-features = ('1A', '2A', '3A', '4A', '6A', '7A', '8A', '9A', '10A', '11A', '13A','19A','20A', '21A', '21B', '22A', '23A', '24A', '25A','25B', '26A','27A', '28A', '29A')
+features = ('1A', '2A', '3A', '4A', '6A', '7A', '8A', '9A', '10A', '11A', '12A', '13A', '14A', '15A', '16A', '17A','19A','20A', '21A', '21B', '22A', '23A', '24A','25B', '26A','27A', '28A', '29A')
 feature_values = {}
 for language in data:
     for feat in data[language]:
@@ -365,7 +364,7 @@ for feat in feature_values:
     i = 0
     for language in data:
         if feat in data[language]:
-            data[language][feat]['value'] = normalized[i, 0]
+            data[language][feat]['value'] = round(float(normalized[i,0]),3)
             i += 1
 
 with open('modified_info_standardized.json', 'w') as f:
